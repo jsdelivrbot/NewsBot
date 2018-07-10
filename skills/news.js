5f6eed2f3ceda84124850906fda6a828910919b4
Glitch (lying-bus) <none>
2018-07-10 17:04:38 +0000

    ☝️💓 Checkpoint ./skills/news.js:1435541/3747

--- skills/news.js
+++ skills/news.js
@@ -9,6 +9,7 @@ through the conversation are chosen based on the user's response.
 
 */
 
+
 module.exports = function(controller) {
 
     controller.hears(['news'], 'direct_message,direct_mention', function(bot, message) {
@@ -16,12 +17,37 @@ module.exports = function(controller) {
         bot.startConversation(message, function(err, convo) {
             convo.say('Hmm let me think...');
 
-            convo.ask('What are you interested in?', function(response, convo) {
+                var today = new Date();
+                var dd = today.getDate();
+                var mm = today.getMonth()+1; //January is 0!
+                var yyyy = today.getFullYear();
 
-                convo.say('I am interested in ' + response.text + ' too');
-                convo.next();
+                if(dd<10) {
+                    dd = '0' + dd
+                } 
 
-            });
+                if(mm<10) {
+                    mm = '0' + mm
+                } 
+
+                today = yyyy + '-' + mm + '-' + dd;
+                document.write(today);
+          
+          var url = 'https://newsapi.org/v2/everything?' +
+          'q=MetLife&' +
+          'from=&' +
+          today +
+          'sortBy=popularity&' +
+          'apiKey=ac625565dfc847019c3369e3c4b3ea73';
+
+          var req = new Request(url);
+
+          fetch(req)
+          
+              .then(function(response) {
+                  console.log(response.json());
+              })
+            
         });
 
     });
