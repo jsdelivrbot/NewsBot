3baa64d18894f0246f7046226b25923b2aae433e
Glitch (lying-bus) <none>
2018-07-16 13:44:04 +0000

    👣😅 Checkpoint ./skills/news.js:1435541/7550

--- skills/news.js
+++ skills/news.js
@@ -20,125 +20,12 @@ module.exports = function(controller) {
 
         bot.startConversation(message, function(err, convo) {
 
-                //gets todays date in the correct format for API
-                var today = new Date();
-                var dd = today.getDate();
-                var mm = today.getMonth()+1; //January is 0!
-                var yyyy = today.getFullYear();
-
-                if(dd<10) {
-                    dd = '0' + dd
-                } 
-
-                if(mm<10) {
-                    mm = '0' + mm
-                }
-
-                today = yyyy + '-' + mm + '-' + dd;
-          
-                //gets the date 1 week ago in correct format for API
-                var oneWeekAgo = new Date();
-                oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
-          
-                dd = oneWeekAgo.getDate();
-                mm = oneWeekAgo.getMonth()+1; //January is 0!
-                yyyy = oneWeekAgo.getFullYear();
-
-                var weekAgo = yyyy + '-' + mm + '-' + dd;
-          
-                newsapi.v2.everything({
-                  
-                  q: 'Insurance',
-                  
-                  from: weekAgo,
-                  
-                  to: today,
-                  
-                  language: 'en',
-                  
-                  sortBy: 'popularity',
-                  
-                  page: 1
-                  
-                }).then(response => {
-                  
-                    if(response.articles[0].title){
-                    
-                      console.log(response.totalResults + " articles found");
-                          
-                      insuranceArticles = '<'+ response.articles[0].url + '|*' + response.articles[0].title + '*>\n*' + 
-                          response.articles[0].source.name + '*\n' + response.articles[0].description + '\n';
-                      
-                      var i = 1;
-                      while(i < 3){
-                        insuranceArticles = insuranceArticles + '<'+ response.articles[i].url + '|*' + response.articles[i].title + '*>\n*' + 
-                          response.articles[i].source.name + '*\n' + response.articles[i].description + '\n';
-                        
-                        i++;
-                      }
-                      
-                      console.log(insuranceArticles);
-                      
-                    } else { 
-                      
-                      console.log('No articles found');
-                      
-                      convo.say('There were no top articles on insurance this week'); 
-                      convo.next();
-                      
-                           }
-                   
-                  
-                });
-          
-                
-                newsapi.v2.everything({
-                  
-                  q: 'MetLife',
-                  
-                  from: weekAgo,
-                  
-                  to: today,
-                  
-                  language: 'en',
-                  
-                  sortBy: 'popularity',
-                  
-                  page: 1
-                  
-                }).then(response => {
-                  
-                    if(response.articles[0].title){
-                    
-                      console.log(response.totalResults + " articles found");
-                          
-                      metArticles = '<'+ response.articles[0].url + '|*' + response.articles[0].title + '*>\n*' + 
-                          response.articles[0].source.name + '*\n' + response.articles[0].description + '\n';
-                      
-                      var i = 1;
-                      while(i < 3){
-                        metArticles = metArticles + '<'+ response.articles[i].url + '|*' + response.articles[i].title + '*>\n*' + 
-                          response.articles[i].source.name + '*\n' + response.articles[i].description + '\n';
-                        
-                        i++;
-                      }
-                      
-                      console.log(metArticles);
-                    
-                    } else { 
-                      
-                      console.log('No articles found');
-                      metArticles = 'No articles on MetLife this week.'
-                      
-                           }
-                   
-                  
-                }); 
-          
+                var metNews = queryToSlack('MetLife', 3);
+                var insuranceNews = queryToSlack('Insurance', 3);
           
                 bot.reply(message, {   
                                 
-                              'text': 'Top Articles on MetLife\n' + metArticles + 'Top Articles on Insurance\n' + insuranceArticles
+                              'text': 'Top Articles on MetLife\n' + metNews + 'Top Articles on Insurance\n' + insuranceArticles
                       });
                       
                       convo.stop();     
@@ -148,5 +35,82 @@ module.exports = function(controller) {
     });
 
 };
+
+
+var queryToSlack = function (q, n) {
+ 
+  var articles;
+  
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yyyy = today.getFullYear();
+
+  if(dd<10) {
+    dd = '0' + dd
+  } 
+
+  if(mm<10) {
+    mm = '0' + mm
+  }
+
+  today = yyyy + '-' + mm + '-' + dd;
+          
+  //gets the date 1 week ago in correct format for API
+  var oneWeekAgo = new Date();
+  oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+          
+  dd = oneWeekAgo.getDate();
+  mm = oneWeekAgo.getMonth()+1; //January is 0!
+  yyyy = oneWeekAgo.getFullYear();
+
+  var weekAgo = yyyy + '-' + mm + '-' + dd;
+  
+  
+  newsapi.v2.everything({
+                  
+                  q: q,
+                  
+                  from: weekAgo,
+                  
+                  to: today,
+                  
+                  language: 'en',
+                  
+                  sortBy: 'popularity',
+                  
+                  page: 1
+                  
+                }).then(response => {
+                  
+                    if(response.articles[0].title){
+                    
+                      console.log(response.totalResults + " articles found");
+                          
+                      articles = '<'+ response.articles[0].url + '|*' + response.articles[0].title + '*>\n*' + 
+                          response.articles[0].source.name + '*\n' + response.articles[0].description + '\n';
+                      
+                      var i = 1;
+                      while(i < n){
+                        articles = articles + '<'+ response.articles[i].url + '|*' + response.articles[i].title + '*>\n*' + 
+                          response.articles[i].source.name + '*\n' + response.articles[i].description + '\n';
+                        
+                        i++;
+                      }
+                      
+                      return articles;
+                    
+                    } else { 
+                      
+                      console.log('No articles found');
+                      articles = 'No articles on MetLife this week.'
+                      
+                      return articles;
+                      
+                           }
+                   
+                }); 
+  
+}
 
 
