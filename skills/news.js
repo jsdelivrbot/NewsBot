d0b22be464c62dc4b306386d0f71fda14956c0a8
Glitch (lying-bus) <none>
2018-07-16 15:57:27 +0000

    🚝😅 Checkpoint ./skills/news.js:1435541/1432

--- skills/news.js
+++ skills/news.js
@@ -102,34 +102,17 @@ function queryToSlack(q, n) {
                       
                       var i = 1;
                       while(i < n){
-                        articles = articles + '<'+ response.articles[i].url + '|*' + response.articles[i].title + '*>\n*' + 
-                          response.articles[i].source.name + '*\n' + response.articles[i].description + '\n';
+                        articles = articles + '
                         
-                        [
                           {
-                              "fallback": "News on " + q + ".",
+                              \"fallback\": \"News on\"' + q + ',
                               "color": color,
-                              "pretext": "Optional text that appears above the attachment block",
-                              "author_name": "Bobby Tables",
-                              "author_link": "http://flickr.com/bobby/",
-                              "author_icon": "http://flickr.com/icons/bobby.jpg",
-                              "title": "Slack API Documentation",
-                              "title_link": "https://api.slack.com/",
-                              "text": "Optional text that appears within the attachment",
-                              "fields": [
-                                  {
-                                      "title": "Priority",
-                                      "value": "High",
-                                      "short": false
-                                  }
-                              ],
-                              "image_url": "http://my-website.com/path/to/image.jpg",
-                              "thumb_url": "http://example.com/path/to/thumb.png",
-                              "footer": "Slack API",
-                              "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+                              "author_name": response.articles[i].source.name,
+                              "title": response.articles[i].title,
+                              "title_link": response.articles[i].url,
+                              "text": response.articles[i].description,
                               "ts": 123456789
-                          }
-                      ]
+                          }';
                         
                         i++;
                       }
